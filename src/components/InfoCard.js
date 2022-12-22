7f1fe6ffa10c75a73a0e30c49c06bc8f9ae853ed
Chris <chunt661@mtroyal.ca>
2022-12-22 13:25:01 -0700

    Added height transition when info card mounts

--- src/components/InfoCard.js
+++ src/components/InfoCard.js
@@ -1,22 +1,68 @@
 import "./InfoCard.css";
 
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
+import { Transition } from "react-transition-group";
+
+const duration = 200;
+
+const defaultStyle = {
+  transition: `height ${duration}ms ease-in-out`,
+  height: 0,
+}
+
 
 export const InfoCard = (props) => {
     const [open, setOpen] = useState(false);
+    const [titleHeight, setTitleHeight] = useState(0);
+    
+    const titleContainerRef = useRef(null);
+    const titleRef = useRef(null);
     
     const handleTitleClick = () => {
         setOpen(!open);
     };
     
+    const transitionStyles = {
+      entering: { height: (titleHeight+8) + "px" },
+      entered:  { height: titleHeight + "px" },
+      exiting:  { height: 0 },
+      exited:  { height: 0 },
+    };
+    
+    useEffect(() => {
+        if (titleRef.current) {
+            console.log(titleRef.current.getBoundingClientRect().height + "px")
+            setTitleHeight(titleRef.current.getBoundingClientRect().height);
+        } else {
+            setTitleHeight(0);
+        }
+        
+        return () => setTitleHeight(0);
+    }, [titleRef]);
+    
     return (
         <div className="infocard-wrapper">
             <div className="infocard-handle" />
             <div className="infocard">
-                <div className="infocard-title"
-                    onClick={handleTitleClick}>
-                    <b>{props.roomName}</b>
+                <Transition nodeRef={titleContainerRef}
+                    in={true} appear={true}
+                    unmountOnExit={true}
+                    timeout={duration}>
+                { state => (
+                <div className="infocard-title-container"
+                    ref={titleContainerRef}
+                    onClick={handleTitleClick}
+                    style={{
+                        ...defaultStyle,
+                        ...transitionStyles[state]
+                    }}>
+                            <div className="infocard-title"
+                                ref={titleRef}>
+                                <b>{props.roomName}</b>
+                            </div>
                 </div>
+                )}
+                </Transition>
                 <div className={"infocard-content-wrapper" + (open ? " open" : " closed")}>
                     <div className="infocard-content">
                         <div className="infocard-subcontent">

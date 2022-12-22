23ec80e70df8474dddd59b8c46b066377c992875
Chris <chunt661@mtroyal.ca>
2022-12-22 14:57:48 -0700

    Added error callback to fetch functions

--- src/api.js
+++ src/api.js
@@ -2,32 +2,52 @@
 const API_PATH = "http://localhost:8080/api";
 const TILE_PATH = "http://localhost:8080/tiles";
 
-export function fetchMapData(mapID, fn) {
+export function fetchMapData(mapID, fn, errFn) {
     fetch(`${API_PATH}/data/${mapID}`)
         .then(res => res.json())
         .then(json => fn(json))
-        .catch(e => console.log(e)); // TODO: proper logging
+        .catch(e => {
+            console.log(e);  // TODO: proper logging
+            if (errFn) {
+                errFn();
+            }
+        });
 }
 
-export function fetchFeatures(mapID, floor, fn) {
+export function fetchFeatures(mapID, floor, fn, errFn) {
     fetch(`${API_PATH}/features/${mapID}/floor/${floor}`)
         .then(res => res.json())
         .then(json => fn(json))
-        .catch(e => console.log(e)); // TODO: proper logging
+        .catch(e => {
+            console.log(e);  // TODO: proper logging
+            if (errFn) {
+                errFn();
+            }
+        });
 }
 
-export function fetchSingleFeature(mapID, featureID, fn) {
+export function fetchSingleFeature(mapID, featureID, fn, errFn) {
     fetch(`${API_PATH}/features/${mapID}/${featureID}`)
         .then(res => res.json())
         .then(json => fn(json))
-        .catch(e => console.log(e)); // TODO: proper logging
+        .catch(e => {
+            console.log(e);  // TODO: proper logging
+            if (errFn) {
+                errFn();
+            }
+        });
 }
 
-export function fetchSearchResults(mapID, query, fn) {
+export function fetchSearchResults(mapID, query, fn, errFn) {
     fetch(`${API_PATH}/search/${mapID}/${query}`)
         .then(res => res.json())
         .then(json => fn(json))
-        .catch(e => console.log(e)); // TODO: proper logging
+        .catch(e => {
+            console.log(e);  // TODO: proper logging
+            if (errFn) {
+                errFn();
+            }
+        });
 }
 
 export function getTileURL(mapID, floor) {

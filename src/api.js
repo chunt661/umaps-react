889f2136e919c4e0264ce1820823f7d949fdf323
Chris <chunt661@mtroyal.ca>
2022-12-23 11:43:08 -0700

    Implemented local storage and consolidated exported functions into two local functions for fetching and loading data

--- src/api.js
+++ src/api.js
@@ -3,53 +3,61 @@ const API_PATH = "http://localhost:8080/api";
 const TILE_PATH = "http://localhost:8080/tiles";
 
 export function fetchMapData(mapID, fn, errFn) {
-    fetch(`${API_PATH}/data/${mapID}`)
-        .then(res => res.json())
-        .then(json => fn(json))
-        .catch(e => {
-            console.log(e);  // TODO: proper logging
-            if (errFn) {
-                errFn();
-            }
-        });
+    fetchData(`data/${mapID}`, fn, errFn);
 }
 
 export function fetchFeatures(mapID, floor, fn, errFn) {
-    fetch(`${API_PATH}/features/${mapID}/floor/${floor}`)
-        .then(res => res.json())
-        .then(json => fn(json))
-        .catch(e => {
-            console.log(e);  // TODO: proper logging
-            if (errFn) {
-                errFn();
-            }
-        });
+    loadData(`features/${mapID}/floor/${floor}`, fn, errFn);
 }
 
 export function fetchSingleFeature(mapID, featureID, fn, errFn) {
-    fetch(`${API_PATH}/features/${mapID}/${featureID}`)
-        .then(res => res.json())
-        .then(json => fn(json))
-        .catch(e => {
-            console.log(e);  // TODO: proper logging
-            if (errFn) {
-                errFn();
-            }
-        });
+    loadData(`features/${mapID}/${featureID}`, fn, errFn);
 }
 
 export function fetchSearchResults(mapID, query, fn, errFn) {
-    fetch(`${API_PATH}/search/${mapID}/${query}`)
-        .then(res => res.json())
-        .then(json => fn(json))
-        .catch(e => {
-            console.log(e);  // TODO: proper logging
-            if (errFn) {
-                errFn();
-            }
-        });
+    fetchData(`search/${mapID}/${query}`)
 }
 
 export function getTileURL(mapID, floor) {
     return `${TILE_PATH}/${mapID}/${floor}/{z}/{x}/{y}`;
+}
+
+/**
+Retrieves data from the API. First attempts to load the data from local 
+storage. If no data is stored, it is fetched from the server instead.
+*/
+function loadData(path, fn, errFn) {
+    const data = localStorage.getItem(path);
+    
+    if (!data) {
+        fetch(`${API_PATH}/${path}`)
+            .then(res => res.json())
+            .then(json => {
+                fn(json);
+                localStorage.setItem(path, JSON.stringify(json));
+            })
+            .catch(e => {
+                console.log(e);  // TODO: proper logging
+                if (errFn) {
+                    errFn();
+                }
+            });
+    } else {
+        fn(JSON.parse(data));
+    }
+}
+
+/**
+Retrieves data from the server.
+*/
+function fetchData(path, fn, errFn) {
+    fetch(`${API_PATH}/${path}`)
+        .then(res => res.json())
+        .then(json => fn(json))
+        .catch(e => {
+            console.log(e);  // TODO: proper logging
+            if (errFn) {
+                errFn();
+            }
+        });
 }

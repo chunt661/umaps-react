07a743957040a1ae63abb2f793ba80aa140d232d
Chris <chunt661@mtroyal.ca>
2022-12-23 12:22:44 -0700

    Comments and cleanup

--- src/api.js
+++ src/api.js
@@ -2,34 +2,60 @@
 const API_PATH = "http://localhost:8080/api";
 const TILE_PATH = "http://localhost:8080/tiles";
 
+/**
+Retrieves high-level information of a map from the API. Data retrieved
+includes the map name, dimensions, number of floors, and other metadata
+related to map display and navigation.
+*/
 export function fetchMapData(mapID, fn, errFn) {
     fetchData(`data/${mapID}`, fn, errFn);
 }
 
+/**
+Retrieves basic information and geometry of all features on a given floor.
+*/
 export function fetchFeatures(mapID, floor, fn, errFn) {
     loadData(`features/${mapID}/floor/${floor}`, fn, errFn);
 }
 
+/**
+Retrieves complete information about a single feature.
+*/
 export function fetchSingleFeature(mapID, featureID, fn, errFn) {
     fetchData(`features/${mapID}/${featureID}`, fn, errFn);
 }
 
+/**
+Retrieves search results from the server.
+*/
 export function fetchSearchResults(mapID, query, fn, errFn) {
     fetchData(`search/${mapID}/${query}`, fn, errFn);
 }
 
+/**
+Returns a path to a specific map tile image.
+*/
 export function getTileURL(mapID, floor) {
     return `${TILE_PATH}/${mapID}/${floor}/{z}/{x}/{y}`;
 }
 
+/**
+Loads map data from local storage.
+*/
 export function loadMapData(mapID) {
     return JSON.parse(localStorage.getItem(`data/${mapID}`));
 }
 
+/**
+Saves map data to local storage.
+*/
 export function saveMapData(mapID, data) {
     localStorage.setItem(`data/${mapID}`, JSON.stringify(data));
 }
 
+/**
+Clears all local storage entries that belong to the given map ID.
+*/
 export function clearLocalData(mapID) {
     for (let i = 0; i < localStorage.length; i++) {
         if (localStorage.key(i).match(`[^\/]+\/${mapID}\/.*`)) {
@@ -39,25 +65,16 @@ export function clearLocalData(mapID) {
 }
 
 /**
-Retrieves data from the API. First attempts to load the data from local 
-storage. If no data is stored, it is fetched from the server instead.
+Loads saved data or fetches it from the server if it is not saved locally.
 */
 function loadData(path, fn, errFn) {
     const data = localStorage.getItem(path);
     
     if (!data) {
-        fetch(`${API_PATH}/${path}`)
-            .then(res => res.json())
-            .then(json => {
-                fn(json);
-                localStorage.setItem(path, JSON.stringify(json));
-            })
-            .catch(e => {
-                console.log(e);  // TODO: proper logging
-                if (errFn) {
-                    errFn();
-                }
-            });
+        fetchData(path, (data) => {
+            fn(data);
+            localStorage.setItem(path, JSON.stringify(data));
+        }, errFn);
     } else {
         fn(JSON.parse(data));
     }
